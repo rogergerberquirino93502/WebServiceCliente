a6bb0fef9770e85a400cbc7629ef18620de8fe10
Roger-Therion <42680508+Roger-Therion@users.noreply.github.com>
2020-10-10 15:48:31 -0600

    ok

--- src/java/Vista/ProductosSP.java
+++ src/java/Vista/ProductosSP.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Vista;
 
 import com.servicios.Productos;
@@ -32,7 +27,6 @@ public class ProductosSP {
         com.servicios.ConsultasWS port = service.getConsultasWSPort();
         return port.listaid(id);
     }
-
     public String actualizar(int id, java.lang.String categoria, java.lang.String nombre, java.lang.String precio, java.lang.String mayoreo, java.lang.String oferta, java.lang.String cantidad) {
         com.servicios.ConsultasWS_Service service = new com.servicios.ConsultasWS_Service();
         com.servicios.ConsultasWS port = service.getConsultasWSPort();
